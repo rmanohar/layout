cf6041f3bc023b7a8d91ebfd84e3c3b038607ffa
Rajit Manohar <rajit.manohar@yale.edu>
2020-03-18 09:30:41 -0400

    updated

--- cells/stk_layout.cc
+++ cells/stk_layout.cc
@@ -1,6 +1,6 @@
 /*************************************************************************
  *
- *  Copyright (c) 2019 Rajit Manohar
+ *  Copyright (c) 2019-2020 Rajit Manohar
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License
