85b952569a846537e2882872b90f5b5f984fe6e3
Rajit Manohar <rajit.manohar@yale.edu>
2021-03-22 13:58:28 -0400

    stk_layout.cc

--- stk_pass.h
+++ stk_pass.h
@@ -1,6 +1,6 @@
 /*************************************************************************
  *
- *  Copyright (c) 2021 Rajit Manohar
+ *  Copyright (c) 2019-2021 Rajit Manohar
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
