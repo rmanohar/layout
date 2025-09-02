3171372ae7299ec386b546a6a66a677a6224a35c
Rajit Manohar <rajit.manohar@yale.edu>
2025-09-02 15:54:34 -0400

    parse alignment labels and draw them as special label names

--- mag.pl
+++ mag.pl
@@ -54,23 +54,34 @@ sub process_rect {
 	}
 	next if ($rect ne "rect") && ($rect ne "inrect") && ($rect ne "outrect");
 	print "box $llx $lly $urx $ury\n";
-	print "paint $mat\n";
-	if ($node ne "#") {
-	    $llx++;
-	    $lly++;
-	    print "box $llx $lly $llx $lly\n";
-	    $node =~ s/\[/\(/g;
-	    $node =~ s/\]/\)/g;
-	    print "label \"$node\" right $mat\n";
-	    $llx--;
-	    $lly--;
-	    if ($rect eq "inrect") {
-		print "port class input\n";
-		print "port make\n";
+        if ($mat eq "\$align") {
+	    if ($node ne "#") {
+		$node =~ /^\$(.):(.*)$/;
+		print "label align_$1_$2\n";
 	    }
-	    elsif ($rect eq "outrect") {
-		print "port class output\n";
-		print "port make\n";
+	    else {
+		print "label align_BBOX\n";
+	    }
+	}
+	else {
+	    print "paint $mat\n";
+	    if ($node ne "#") {
+		$llx++;
+		$lly++;
+		print "box $llx $lly $llx $lly\n";
+		$node =~ s/\[/\(/g;
+		$node =~ s/\]/\)/g;
+		print "label \"$node\" right $mat\n";
+		$llx--;
+		$lly--;
+		if ($rect eq "inrect") {
+		    print "port class input\n";
+		    print "port make\n";
+		}
+		elsif ($rect eq "outrect") {
+		    print "port class output\n";
+		    print "port make\n";
+		}
 	    }
 	}
     }
